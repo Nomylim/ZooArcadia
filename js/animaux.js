c916f722f59c76d1a123e8a72ab41526906a8c22
Nomy <noemielimbasse@gmail.com>
2024-07-19 13:48:32 +0200

    création tableau avec toutes les consommations de nourriture

--- js/animaux.js
+++ js/animaux.js
@@ -4,7 +4,7 @@ const InputAnimal = document.getElementById('AnimalInput');
 const InputNourriture = document.getElementById('NourritureInput');
 const InputGrammage = document.getElementById('GrammageInput');
 const InputDate = document.getElementById('DateInput');
-const InputHeure = document.getElementById('HeureInput'); 
+const InputHeure = document.getElementById('HeureInput');
 const btnNourriture = document.getElementById('btnNourriture');
 const formNourriture = document.getElementById('formulaireNourriture');
 
@@ -18,7 +18,7 @@ InputDate.addEventListener("input", validateFormNourriture);
 InputHeure.addEventListener("input", validateFormNourriture);
 InputAnimal.addEventListener("change", validateFormNourriture);
 
-function validateFormNourriture(){
+function validateFormNourriture() {
 
     const animalOk = validateRequired(InputAnimal);
     const grammageOk = validateGrammage(InputGrammage);
@@ -27,45 +27,45 @@ function validateFormNourriture(){
     const heureOk = validateRequired(InputHeure);
 
 
-    if(grammageOk && nourritureOk && dateOk && heureOk && animalOk){
+    if (grammageOk && nourritureOk && dateOk && heureOk && animalOk) {
         btnNourriture.disabled = false;
         errorMessage.style.display = 'none';
     }
-    else{
+    else {
         btnNourriture.disabled = true;
         errorMessage.style.display = 'block';
     }
 }
 
-function validateGrammage(input){
+function validateGrammage(input) {
     const grammageRegex = /^[1-9]\d*$/;
     const grammageUser = input.value.trim();
-    if(grammageUser.match(grammageRegex)){
+    if (grammageUser.match(grammageRegex)) {
         input.classList.add("is-valid");
         input.classList.remove("is-invalid");
-        return true 
+        return true
     }
-    else{
+    else {
         input.classList.remove("is-valid");
         input.classList.add("is-invalid");
         return false;
     }
 }
 
-function validateRequired(input){
-    if(input.value.trim() !== ''){
+function validateRequired(input) {
+    if (input.value.trim() !== '') {
         input.classList.add("is-valid");
         input.classList.remove("is-invalid");
-        return true; 
+        return true;
     }
-    else{
+    else {
         input.classList.remove("is-valid");
         input.classList.add("is-invalid");
         return false;
     }
 }
 
-function EnregistrerNourriture(){
+function EnregistrerNourriture() {
     let dataForm = new FormData(formNourriture);
 
     const myHeaders = new Headers();
@@ -86,22 +86,62 @@ function EnregistrerNourriture(){
         redirect: "follow"
     };
 
-    fetch(apiUrl+"nourriture", requestOptions)
-    .then((response) => {
-        if(response.ok){
-            return response.json();
-        }
-        else {
-            throw new Error("Erreur lors de la création de la consommation");
-        }
-    })
-    .then((result) => {
-        alert("Bravo, consommation ajouté !");
-        location.reload();
-    })
-    .catch((error) => {
-        alert(error.message);
-    })
+    fetch(apiUrl + "nourriture", requestOptions)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            else {
+                throw new Error("Erreur lors de la création de la consommation");
+            }
+        })
+        .then((result) => {
+            alert("Bravo, consommation ajouté !");
+            location.reload();
+        })
+        .catch((error) => {
+            alert(error.message);
+        })
 }
 
+$(document).ready(function () {
+    // Initialiser la table avec des options
+    $('#table').bootstrapTable({
+        search: true,
+        filterControl: true,
+        showExport: true,
+        clickToSelect: true,
+        sortable: true,
+        toolbar: '#toolbar'
+    });
 
+    fetch(apiUrl + 'nourriture_all')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur réseau');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Data received:', data);
+            data.forEach(item => {
+
+                const row = document.createElement('tr');
+                const animalId = (item.animal && item.animal.id!==undefined)  ? item.animal.id : 'N/A';
+            
+                row.innerHTML = `
+                <td>${item.id}</td>
+                <td>${animalId}</td>
+                <td>${item.nom}</td>
+                <td>${item.grammage}</td>
+                <td>${item.date}</td>
+                <td>${item.heure}</td>
+            `;
+                document.getElementById('container').appendChild(row);
+            });
+            $('#table').bootstrapTable('load', data);
+        })
+        .catch(error => {
+            console.error('Il y a eu un problème avec la requête fetch:', error);
+        });
+});
--- js/rapportVeto.js
+++ js/rapportVeto.js
@@ -129,45 +129,6 @@ fetch(apiUrl+'rapportveterinaires_all')
     })
     .then(data => {
         $('#table').bootstrapTable('load', data);
-        /*
-        // Supposons que data est un tableau d'objets avec des propriétés "id", "animal", "nourriture", "grammage", "date", "etatanimal" et "description"
-        const container = document.getElementById('container');
-        container.innerHTML = ''; // Réinitialise le contenu
-
-        data.forEach(rapport => {
-            const Element = document.createElement('tr');
-            Element.classList.add('rapport');
-
-            const containerID = document.createElement('td');
-            containerID.textContent = rapport.id;
-            Element.appendChild(containerID);
-
-            const containerAnimal = document.createElement('td');
-            containerAnimal.textContent = rapport.animal;
-            Element.appendChild(containerAnimal);
-
-            const containerNourriture = document.createElement('td');
-            containerNourriture.textContent = rapport.nourriture;
-            Element.appendChild(containerNourriture);
-
-            const containerGrammage = document.createElement('td');
-            containerGrammage.textContent = rapport.grammage;
-            Element.appendChild(containerGrammage);
-
-            const containerDate = document.createElement('td');
-            containerDate.textContent = rapport.date;
-            Element.appendChild(containerDate);
-
-            const containerEtat = document.createElement('td');
-            containerEtat.textContent = rapport.etatanimal;
-            Element.appendChild(containerEtat);
-
-            const containerDescription = document.createElement('td');
-            containerDescription.textContent = rapport.description;
-            Element.appendChild(containerDescription);
-
-            container.appendChild(Element);
-        });*/
     })
     .catch(error => {
         console.error('Il y a eu un problème avec la requête fetch:', error);
