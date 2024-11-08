db6425ad82fe746f885146d4d86622067fe133c5
Nomy <noemielimbasse@gmail.com>
2024-11-08 11:42:13 +0100

    modif index.js

--- index.js
+++ index.js
@@ -11,18 +11,7 @@ const port = process.env.PORT || 3000;
 // Configurer un dossier public pour les fichiers statiques
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Définir une route simple pour la page d'accueil
-app.get('/', (req, res) => {
-    res.send('Bienvenue sur mon application déployée avec Heroku !');
-});
 
-// Définir une route de test
-app.get('/services', (req, res) => {
-    res.json({
-        message: 'Bienvenue dans les services de l\'application',
-        version: '1.0.0'
-    });
-});
 
 // Démarrer le serveur
 app.listen(port, () => {
