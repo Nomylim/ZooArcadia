b963bff83b2b4e6e4e4347952586a99cd43e4792
Nomy <noemielimbasse@gmail.com>
2024-11-24 14:03:19 +0100

    recréation de dirname et fileneame

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
+import { fileURLToPath } from 'url'; // Pour recréer __dirname
+import { dirname } from 'path'; // Pour recréer __dirname
+
+// Recréer __dirname pour les modules ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // Importer dynamiquement express et d'autres modules
 import('express').then(expressModule => {
-    const express = expressModule.default;  // Récupère la fonction express du module importé
+    const express = expressModule.default; // Récupère la fonction express du module importé
     import('path').then(pathModule => {
-        const path = pathModule;  // Récupère 'path' depuis l'import dynamique
+        const path = pathModule; // Récupère 'path' depuis l'import dynamique
 
         // Importer le fichier router.js
         import('./Router/router.js').then(routerModule => {
-            const { allRoutes } = routerModule;  // Récupérer les routes du fichier router.js
+            const { allRoutes } = routerModule; // Récupérer les routes du fichier router.js
 
             // Initialiser l'application Express
             const app = express();
 
-            // Définir un port pour l'application (port dynamique pour Heroku)
+            // Définir un port pour l'application (port dynamique pour Railway ou Heroku)
             const port = process.env.PORT || 3000;
 
             // Configurer un dossier public pour les fichiers statiques
